c287de2aa936b7676a53ba18ff847d48e4e75cba
Valentin Chassignol <Vinetosdev@gmail.com>
2020-03-11 10:56:42 +0100

    feat: Add Buefy and FontAwesome

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,61 +1,64 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <section>
+
+    <div class="buttons">
+      <b-button type="is-primary">Primary</b-button>
+
+      <b-button type="is-success">Success</b-button>
+
+      <b-button type="is-danger">Danger</b-button>
+
+      <b-button type="is-warning">Warning</b-button>
+
+      <b-button type="is-info">Info</b-button>
+
+      <b-button type="is-link">Link</b-button>
+
+      <b-button type="is-light">Light</b-button>
+
+      <b-button type="is-dark">Dark</b-button>
+
+      <b-button type="is-text">Text</b-button>
+    </div>
+
+    <div class="buttons">
+      <b-button disabled>Disabled</b-button>
+
+      <b-button loading>Loading</b-button>
+
+      <b-button rounded>Rounded</b-button>
+    </div>
+
+    <div class="buttons">
+      <b-button type="is-primary" outlined>Outlined</b-button>
+      <b-button type="is-success" outlined>Outlined</b-button>
+      <b-button type="is-danger" outlined>Outlined</b-button>
+      <b-button type="is-warning" outlined>Outlined</b-button>
+    </div>
+
+    <div class="buttons">
+      <b-button type="is-primary" expanded>Expanded</b-button>
+    </div>
+
+    <div class="notification is-primary">
+      <b-button type="is-primary"
+                inverted>
+        Inverted
+      </b-button>
+
+      <b-button type="is-primary"
+                inverted
+                outlined>
+        Invert Outlined
+      </b-button>
+    </div>
+
+  </section>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String,
-  },
-};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <section>
+    <div class="buttons">
+      <b-button size="is-small"
+                icon-left="github-circle">
+        Add
+      </b-button>
+
+      <b-button icon-left="github">
+        Add
+      </b-button>
+
+      <b-button size="is-medium"
+                icon-left="github-circle">
+        Add
+      </b-button>
+
+      <b-button size="is-large"
+                icon-left="github-circle">
+        Add
+      </b-button>
+    </div>
+
+    <div class="buttons">
+      <b-button type="is-danger"
+                icon-left="delete">
+        Delete
+      </b-button>
+
+      <b-button type="is-danger"
+                icon-right="delete">
+        Delete
+      </b-button>
+
+      <b-button type="is-danger"
+                icon-right="delete" />
+    </div>
+
+  </section>
 </template>
